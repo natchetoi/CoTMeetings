0d2097da41742856283ade8eeca18f6b24d57336
Yuri Natchetoi <natchetoi@gmail.com>
2016-10-28 22:11:59 +0000

    Updated from Eclipse

---
+++ CoTMeetings/model/mockData.js
@@ -0,0 +1,120 @@
+function loadDataModel() {
+return  [{
+"Description" : "IT Boardroom S1",
+"RoomID" : "ba5d6332-92ac-4324-a62e-fe7164514407",
+"RoomName" : "IT Boardroom S1",
+"Capacity": "5",
+"x": "",
+"y": "",
+"Building" :"MH",
+"Floor" : "15" 
+},{
+"Description" : "IT Boardroom S2",
+"RoomID" : "da29dfd1-fe18-40fb-9052-97e299340c8c",
+"RoomName" : "IT Boardroom S2",
+"Capacity": "5",
+"x":"",
+"y":"",
+"Building" :"MH",
+"Floor" : "15" 
+},{
+"Description" : "IT Boardroom S3",
+"RoomID" : "39ee1f8e-a287-48e1-b6d7-5b33131840fb",
+"RoomName" : "IT Boardroom S3",
+"Capacity": "5",
+"x":"",
+"y":"",
+"Building" :"MH",
+"Floor" : "15" 
+},{
+"Description" : "IT Boardroom S4",
+"RoomID" : "fbc8dc05-1557-4ccf-af28-d7d10bddda38",
+"RoomName" : "IT Boardroom S4",
+"Capacity": "5",
+"x":"",
+"y":"",
+"Building" :"MH",
+"Floor" : "15" 
+},{
+"Description": "MH 15 - Adelaide Meeting Room",
+"RoomID" : "1a775e79-c1e1-479b-be90-6704d16b48b1",
+"RoomName" : "Adelaide Meeting Room",
+"Capacity": "5",
+"x":"",
+"y":"",
+"Building" :"MH",
+"Floor" : "15" 
+},{
+"Description": "MH 15 - Berkeley Meeting Room",
+"RoomID" : "2fb16a9c-c4b2-4fa8-a0bc-f81f189acdb8",
+"RoomName" : "Berkeley Meeting Room",
+"Capacity": "5",
+"x":"",
+"y":"",
+"Building" :"MH",
+"Floor" : "15" 
+},{
+"Description": "MH 15 - Duncan Meeting Room",
+"RoomID" : "adf3b026-9a85-494f-b69c-08e328ebdc8d",
+"RoomName" : "Duncan Meeting Room",
+"Capacity": "5",
+"x":"",
+"y":"",
+"Building" :"MH",
+"Floor" : "15" 
+},{
+"Description": "MH 15 - Frederick Meeting Room",
+"RoomID" : "e774d73c-ddbf-4195-8125-2077f005c2e4",
+"RoomName" : "Frederick Meeting Room",
+"Capacity": "5",
+"x":"",
+"y":"",
+"Building" :"MH",
+"Floor" : "15" 
+},{
+"Description": "MH 15 - Jarvis Meeting Room",
+"RoomID" : "6cb32f74-8c46-4faa-8da8-766c02591729",
+"RoomName" : "Jarvis Meeting Room",
+"Capacity": "5",
+"x":"",
+"y":"",
+"Building" :"MH",
+"Floor" : "15" 
+},{
+"Description": "MH 15 - Pearl Meeting Room",
+"RoomID" : "5345c586-ef20-47db-be4c-3298202748e4",
+"RoomName" : "Pearl Meeting Room",
+"Capacity": "5",
+"x":"",
+"y":"",
+"Building" :"MH",
+"Floor" : "15" 
+},{
+"Description": "MH 15 - Richmond Meeting Room",
+"RoomID" : "cab41bd6-a74e-4bde-acf8-cb38295c0b64",
+"RoomName" : "Richmond Meeting Room",
+"Capacity": "5",
+"x":"",
+"y":"",
+"Building" :"MH",
+"Floor" : "15" 
+},{
+"Description": "MH 15 - Simcoe Meeting Room",
+"RoomID" : "28cb401b-2ddc-4dec-84c4-70e425460e10",
+"RoomName" : "Simcoe Meeting Room",
+"Capacity": "5",
+"x":"",
+"y":"",
+"Building" :"MH",
+"Floor" : "15" 
+},{
+"Description": "MH 15 - Wellington Meeting Room",
+"RoomID" : "860ae2ee-2620-4035-b36b-c4ff67d1124a",
+"RoomName" : "Wellington Meeting Room",
+"Capacity": "5",
+"x":"",
+"y":"",
+"Building" :"MH",
+"Floor" : "15" 
+}];
+}
